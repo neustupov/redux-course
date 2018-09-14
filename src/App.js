8ff73bb2de4ec8bc82d6e1c54e7f395e19d21cf9
Dmitry_Neustupov <neustupov@yandex.ru>
2018-09-14 08:41:32 +0300

    added styles, actions, components, reducers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import './App.css'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+import {User} from './components/User'
+import {Page} from './components/Page'
+
+import {setYear} from './actions/PageActions'
+
+import './containers/App.css'
 
 class App extends Component {
     render() {
+        const {user, page, setYearAction} = this.props;
         return (
             <div className="App">
                 <header className="App-header">
                     <h1 className="App-title">Мой топ фото</h1>
                 </header>
-                <p className="App-intro">Здесь будут мои самые залайканые фото</p>
-                <p>Меня зовут: {this.props.user}</p>
+                <User name={user.name}/>
+                <Page photos={page.photos} year={page.year} setYear={setYearAction}/>
             </div>
         )
     }
 }
 
 const mapStateToProps = store => {
-    console.log(store);
     return {
         user: store.user,
+        page: store.page
     }
 };
 
-export default connect(mapStateToProps)(App)
+const mapDispatchToProps = dispatch => {
+    return {
+        setYearAction: year => dispatch(setYear(year)),
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
